8a93d8d8ff22c49dc8fdea5499646e46adb1db96
FUGAMARU <gofugashi@gmail.com>
2022-12-14 21:14:24 +0900

    [Add] 説明画面を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { useColorJunction } from "./useColorJunction"
 
 // Chakra UI Components
-import { Box, Flex, Text, VStack, HStack } from "@chakra-ui/react"
+import { Box, Flex, Text, VStack, HStack, useDisclosure } from "@chakra-ui/react"
 
 // Custom Components
 import Tile from "./Tile"
@@ -14,11 +14,14 @@ import { faRotateLeft, faInfo } from "@fortawesome/free-solid-svg-icons"
 // Type
 import { hex } from "./ts/types/Color"
 
+const Link = (props: { url: string, children: string }) => <Box as="span" textDecoration="underline" _hover={{ color: "#cb94e3" }}><a href={props.url} target="_blank" rel="noopener">{props.children}</a></Box>
+
 const App = () => {
   const height = 15
   const width = 15
 
   const { grid, pieces, gameState, handleTileClick, handleResetButtonClick } = useColorJunction({ height: height, width: width })
+  const { isOpen: isInfoOpen, onOpen: openInfo, onClose: closeInfo } = useDisclosure()
 
   if (grid.flat().length === height * width) {
     return (
@@ -49,19 +52,31 @@ const App = () => {
               )
             })}
           </VStack>
-          {gameState !== "Playing" ?
-            <Flex h="100%" w="100%" position="absolute" top="0" left="0" justify="center" align="center" bg="whiteAlpha.700">
+          {gameState !== "Playing" && !!!isInfoOpen ?
+            <Flex h="100%" w="100%" position="absolute" top="0" left="0" justify="center" align="center" bg="whiteAlpha.800">
               <Text fontSize="20px" fontWeight="regular" textAlign="center">{gameState}</Text>
             </Flex>
             : null
           }
+          {isInfoOpen ?
+            <Flex h="100%" w="100%" position="absolute" top="0" left="0" flexDirection="column" justify="center" align="center" textAlign="center" bg="whiteAlpha.800" onClick={closeInfo}>
+              <Text fontSize="20px" fontWeight="semibold">Color Junction</Text>
+              <Box fontSize="13px" lineHeight="18px">
+                <Text>This game is played by erasing clumps of two or more pieces of the same color attached to each other, and the game is completed when all the pieces are finally erased.</Text>
+                <Text my="5px">Using React, the mini-game "Color Junction" that was playable on <Link url="https://en.wikipedia.org/wiki/IGoogle">iGoogle</Link> which ended in 2013, was revived in 2022.</Text>
+                <Text>Developer: <Link url="https://fugamaru.com/">FUGAMARU</Link></Text>
+              </Box>
+              <Box mt="3px" px="2px" fontSize="5px" _hover={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }} cursor="pointer" onClick={closeInfo}>Click to Close</Box>
+            </Flex>
+            : null
+          }
         </Box>
-        <Flex h="20px" px="3px" justify="space-between" align="center" bg="#cccccc" borderTop="solid 1px #888888">
+        <Flex h="20px" px="3px" justify="space-between" align="center" bg="#cccccc" borderTop="solid 1px #888888" onClick={() => { if (isInfoOpen) closeInfo() }}>
           <Flex align="center">
             <Flex h="14px" w="14px" mr="1.5px" justify="center" align="center" bg="#9e9e9e" borderRadius="50%" cursor="pointer" onClick={handleResetButtonClick} title="reset">
               <FontAwesomeIcon icon={faRotateLeft} color="white" width="10px" height="10px"></FontAwesomeIcon>
             </Flex>
-            <Flex h="14px" w="14px" ml="1.5px" justify="center" align="center" bg="#9e9e9e" borderRadius="50%" cursor="pointer">
+            <Flex h="14px" w="14px" ml="1.5px" justify="center" align="center" bg="#9e9e9e" borderRadius="50%" cursor="pointer" onClick={openInfo} title="info">
               <FontAwesomeIcon icon={faInfo} color="white" width="3.5px" height="3.5px"></FontAwesomeIcon>
             </Flex>
           </Flex>
